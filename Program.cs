a9e5b5dba4f5ceb6e37363592343cbe672c74913
Stellshi <simplepro47@gmail.com>
2021-05-30 07:52:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Trigonometry
+{
+    class Trigonometry
+    {
+        static void Main()  {
+            Double theta; //угол в радианах
+
+            for (
+                theta = 0.1; 
+                theta <= 1.0;
+                theta = theta + 0.1)  
+            {
+                Console.WriteLine("Синус угла " + theta + " равен " + Math.Sin(theta));
+                Console.WriteLine("Косинус угла " + theta + " равен " + Math.Cos(theta));
+                Console.WriteLine("тангенс угла " + theta  + " равен " + Math.Tan (theta));
+                Console.WriteLine();
+                Console.ReadKey();
+
+
+
+            }
+       }
+       
+            
+        }
+    }
+
